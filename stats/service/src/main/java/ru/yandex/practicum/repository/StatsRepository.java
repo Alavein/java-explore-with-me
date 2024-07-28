eb53a55282de024b9315e5fd9bd8473bb6362768
Полина Орлова <cantatcaelum@yandex.ru>
2024-07-28 17:37:17 +0300

    fix: рпавка

--- stats/service/src/main/java/ru/yandex/practicum/repository/StatsRepository.java
+++ stats/service/src/main/java/ru/yandex/practicum/repository/StatsRepository.java
@@ -9,21 +9,21 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsRepository extends JpaRepository<Stats, Integer> {
-    @Query("SELECT new ru.practicum.model.ViewStats(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
+    @Query("SELECT new ru.yandex.practicum.ViewStatsDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM Stats AS s " +
             "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
             "GROUP BY s.app, s.uri " +
             "ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<ViewStatsDto> getAllStatsIp(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(s.app, s.uri, COUNT(s.ip)) " +
+    @Query("SELECT new ru.yandex.practicum.ViewStatsDto(s.app, s.uri, COUNT(s.ip)) " +
             "FROM Stats AS s " +
             "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
             "GROUP BY s.app, s.uri " +
             "ORDER BY COUNT(s.ip) DESC")
     List<ViewStatsDto> getAllStats(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
+    @Query("SELECT new ru.yandex.practicum.ViewStatsDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
             "FROM Stats AS s " +
             "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
             "AND s.uri IN (?3) " +
@@ -31,7 +31,7 @@ public interface StatsRepository extends JpaRepository<Stats, Integer> {
             "ORDER BY COUNT(DISTINCT s.ip) DESC")
     List<ViewStatsDto> getStatsByUrisIp(LocalDateTime start, LocalDateTime end, List<String> uri);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(s.app, s.uri, COUNT(s.ip)) " +
+    @Query("SELECT new ru.yandex.practicum.ViewStatsDto(s.app, s.uri, COUNT(s.ip)) " +
             "FROM Stats AS s " +
             "WHERE s.timestamp BETWEEN ?1 AND ?2 " +
             "AND s.uri IN (?3) " +
